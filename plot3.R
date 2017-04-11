1c1335dbcae310d87008e9a1750b5f885af932ed
zrx09 <zhuangrx.09@gmail.com>
2017-04-11 09:28:19 +0800

    graph commit

--- plot3.R
+++ plot3.R
@@ -1,19 +1,19 @@
 fileURL<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileURL,"Electric power consumption.zip",method="curl")
 unzip("Electric power consumption.zip")
-data<-read.csv("household_power_consumption.txt",header=T,sep=";",na.strings="?")
+dataset<-read.csv("household_power_consumption.txt",header=T,sep=";",na.strings="?")
 
 datetime<-paste(data$Date,data$Time)
 datetime<-strptime(datetime,format="%d/%m/%Y %H:%M:%S")
-data1<-cbind(data,datetime)
-data1$Date<-as.Date(data1$Date,format="%d/%m/%Y")
-data2<-data1[which((data1$Date=="2007-02-01")|(data1$Date=="2007-02-02")),]
+dataset1<-cbind(data,datetime)
+dataset1$Date<-as.Date(dataset1$Date,format="%d/%m/%Y")
+dataset2<-dataset1[which((dataset1$Date=="2007-02-01")|(dataset1$Date=="2007-02-02")),]
 
-png("plot3.png")
+png("plot3.png",width=480,height=480)
 
-plot(data2$datetime,data2$Sub_metering_1,type='l',ylab = "Energy sub metering",xlab="")
-points(data2$datetime,data2$Sub_metering_2,type='l',col="red")
-points(data2$datetime,data2$Sub_metering_3,type='l',col="blue")
+plot(dataset2$datetime,dataset2$Sub_metering_1,type='l',ylab = "Energy sub metering",xlab="")
+points(dataset2$datetime,dataset2$Sub_metering_2,type='l',col="red")
+points(dataset2$datetime,dataset2$Sub_metering_3,type='l',col="blue")
 legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("red","blue","black"),cex=0.75)
 
 dev.off()
